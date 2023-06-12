d805dbd4354584038d91b54bc546d86f600f294d
aberrantian <aberrantian@gmail.com>
2023-06-12 13:26:10 +0100

    setup js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+let myLibrary = [];
+
+function Book(title, author, date) {
+    this.title = title;
+    this.author = author;
+    this.date = date;
+}
+
+function addBookToLibrary(title, author, date) {
+    let newBook = new Book(title, author, date);
+    
+    myLibrary.push(newBook);
+}
+
+// input setup
+const currentYear = new Date().getFullYear();
+const date = document.getElementById('date');
+date.max = currentYear;
+date.value = currentYear;
