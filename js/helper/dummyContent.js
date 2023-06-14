c0037ee70802db2806d1c22933bc4e96eca827e4
aberrantian <aberrantian@gmail.com>
2023-06-15 00:51:59 +0100

    update dummy content

--- js/helper/dummyContent.js
+++ js/helper/dummyContent.js
@@ -2,7 +2,7 @@ import { Book } from "../objects/book.js";
 
 export let dummyContent = [
     new Book('The Hobbit', 'J. R. R Tolkien', 310, 1937, true),
-    new Book('On Writing', 'Stephen King', 384, 2001, true),
+    new Book('On Writing: A Memoir of the Craft', 'Stephen King', 384, 2001, true),
     new Book('The Witcher: Blood of Elves', 'Andrzej Sapkowski', 433, 1994, false),
     new Book('Neuromancer', 'William Gibson', 271, 1984, false)
 ]
