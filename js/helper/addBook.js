f9c17e9e445caa10406af6ccf4608837fa5913f9
aberrantian <aberrantian@gmail.com>
2023-06-15 00:35:02 +0100

    create addBook helper function

---
+++ js/helper/addBook.js
@@ -0,0 +1,12 @@
+import { books } from "../init.js";
+import { Book } from "../objects/book.js";
+
+export function addBook(title, author, pages, date, read) {
+    if (arguments.length == 5) {
+        const newBook = new Book(title, author, pages, date, read);
+    } else {
+        const newBook = new Book();
+    }
+
+    books.push(newBook);
+}
