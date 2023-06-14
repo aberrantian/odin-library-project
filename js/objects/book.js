50cad33ed15548b578fad7caf261929743f4e4d1
aberrantian <aberrantian@gmail.com>
2023-06-15 00:52:12 +0100

    fix book.js

--- js/objects/book.js
+++ js/objects/book.js
@@ -1,7 +1,7 @@
 export function Book(title = 'title', author = 'author', pages = 300, date = 2000, read = false) {
-    if (arguments.length == 5) {
-        for (let index = 0; index < arguments.length; index++) {
-            this.arguments[index] = arguments[index];
-        }
-    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.date = date;
+    this.read = read;
 }
