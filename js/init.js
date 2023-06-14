5bf1d78cf180976f3fa5539c51ec2038cfc6ccb4
aberrantian <aberrantian@gmail.com>
2023-06-15 00:53:49 +0100

    init books array with dummy content

--- js/init.js
+++ js/init.js
@@ -1,5 +1,16 @@
 import { screenTemplates, setScreen } from "./helper/screenmgr.js";
+import { dummyContent } from "./helper/dummyContent.js";
 
+// setup library datebase
+export let books = [];
+dummyContent.forEach(book => {
+    books.push(book);
+});
+
+// setup table contents;
+// export let table = [];
+
+// setup screens
 export function init() {
     for (const key in screenTemplates) {
         document.body.appendChild(screenTemplates[key]());
