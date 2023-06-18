8e887f27fa5273626538e0d6bdd1b1faf180e872
aberrantian <aberrantian@gmail.com>
2023-06-18 22:30:11 +0100

    reformat updateLibrary tabs

--- js/helper/updateLibrary.js
+++ js/helper/updateLibrary.js
@@ -19,29 +19,29 @@ export function updateLibrary(library = doc.get('library')) {
     // populate library
     let cards = [];
     
-        books.forEach(book => {
-            let card = doc.new('div');
-            const title = p(`${book.title}`);
-            const author = p(`${book.author}`);
-            const pages = p(`pages: ${book.pages}`);
-            const date = p(`published in ${book.date}`);
-    
-            if (book.read) {
-                card.classList.add('read');
-            }
-    
-            card.classList.add('card');
-    
-            card.append(title, author, pages, date);
-            cards.push(card);
-        });
-    
-        for (let index = 0; index < cards.length; index++) {
-            const card = cards[index];
-            card.id = index + 1;
+    books.forEach(book => {
+        let card = doc.new('div');
+        const title = p(`${book.title}`);
+        const author = p(`${book.author}`);
+        const pages = p(`pages: ${book.pages}`);
+        const date = p(`published in ${book.date}`);
+
+        if (book.read) {
+            card.classList.add('read');
         }
-        
-        cards.forEach(card => {
-            library.appendChild(card);
-        })
+
+        card.classList.add('card');
+
+        card.append(title, author, pages, date);
+        cards.push(card);
+    });
+
+    for (let index = 0; index < cards.length; index++) {
+        const card = cards[index];
+        card.id = index + 1;
+    }
+    
+    cards.forEach(card => {
+        library.appendChild(card);
+    })
 }
