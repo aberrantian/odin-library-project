41c86a6f21d99906a15a5a9f6b3932781df5e1f6
aberrantian <aberrantian@gmail.com>
2023-06-21 13:44:24 +0100

    update dummy content

--- js/helper/dummyContent.js
+++ js/helper/dummyContent.js
@@ -2,7 +2,17 @@ import { Book } from "../objects/book.js";
 
 export let dummyContent = [
     new Book('The Hobbit', 'J. R. R Tolkien', 310, 1937, true),
+    new Book('The Fellowship Of The Ring', 'J. R. R Tolkien', 423, 1954, false),
+    new Book('The Two Towers', 'J. R. R Tolkien', 352, 1954, false),
+    new Book('The Return Of The King', 'J. R. R Tolkien', 416, 1955, false),
     new Book('On Writing: A Memoir of the Craft', 'Stephen King', 384, 2001, true),
-    new Book('The Witcher: Blood of Elves', 'Andrzej Sapkowski', 433, 1994, false),
-    new Book('Neuromancer', 'William Gibson', 271, 1984, false)
+    new Book('The Witcher: Blood of Elves', 'Andrzej Sapkowski', 320, 1994, false),
+    new Book('The Witcher: Time of Contempt', 'Andrzej Sapkowski', 352, 1995, false),
+    new Book('The Witcher: Baptism of Fire', 'Andrzej Sapkowski', 352, 1996, false),
+    new Book('The Witcher: The Tower of the Swallow', 'Andrzej Sapkowski', 464, 1997, false),
+    new Book('The Witcher: The Lady of the Lake', 'Andrzej Sapkowski', 544, 1999, false),
+    new Book('Neuromancer', 'William Gibson', 271, 1984, false),
+    new Book('Count Zero', 'William Gibson', 256, 1986, false),
+    new Book('Monsa Lisa Overdrive', 'William Gibson', 260, 1988, false),
+    new Book('Bacchanal', 'Veronica G. Henry', 347, 2021, false)
 ]
