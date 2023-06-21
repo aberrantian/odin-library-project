b5964d53fff5f50facc5a5e6685578128f50cb28
aberrantian <aberrantian@gmail.com>
2023-06-21 13:14:40 +0100

    add remove books function

--- js/screens/remove-screen.js
+++ js/screens/remove-screen.js
@@ -1,6 +1,7 @@
 import { doc } from "../helper/doc.js";
-import { removeMode } from "../../main.js";
+import { removeMode, books } from "../../main.js";
 import { toggleScreen } from "../helper/screenmgr.js";
+import { updateLibrary } from "../helper/updateLibrary.js";
 
 export function createRemoveScreen() {
     const removeScreen = doc.new('div');
@@ -32,6 +33,13 @@ export function createRemoveScreen() {
     confirmRemoveBtn.type = 'button';
     confirmRemoveBtn.textContent = 'Confirm';
     confirmRemoveBtn.id = 'confirmRemoveBtn';
+    confirmRemoveBtn.addEventListener('click', () => {
+        const target = books[removeMode.target.id];
+        books.splice(target, 1);
+        updateLibrary();
+        removeMode.toggle();
+        toggleScreen('removeScreen');
+    })
 
     rmvBtnContainer.append(cancelRemoveBtn, confirmRemoveBtn);
     removeScreenContent.append(confirmMessage, bookInfo, rmvBtnContainer);
