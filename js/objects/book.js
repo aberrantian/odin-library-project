16da2c362a3f15870652089f992d81adf86ad21a
aberrantian <aberrantian@gmail.com>
2023-10-26 20:23:11 +0100

    use class for book object

--- js/objects/book.js
+++ js/objects/book.js
@@ -1,7 +1,9 @@
-export function Book(title = 'title', author = 'author', pages = 300, date = 2000, read = false) {
+export class Book {
+  constructor(title, author, pages, date, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.date = date;
     this.read = read;
+  }
 }
